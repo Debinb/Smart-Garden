b088aa5c1f5f2ff5bbe67052cf9398852d082507
Debin Babykutty <96095584+Debinb@users.noreply.github.com>
2024-06-24 13:55:32 +0000

    Analog Comparator code

--- Sensor.c
+++ Sensor.c
@@ -18,12 +18,28 @@
 #include "sortEvent.h"
 #include "getInput.h"
 
+#define C0_PIN PORTC,7
 #define C0_MASK 128         // Port C7 for Analog Comparator
 
 void initHW()
 {
     initSystemClockTo40Mhz();
 
+    enablePort(PORTC);
+    selectPinAnalogInput(C0_PIN);
+
     initUart0();
     setUart0BaudRate(115200, 40e6);
+    _delay_cycles(3);
+
+    COMP_ACREFCTL_R |= COMP_ACREFCTL_EN;                 // Enable the reference module
+    COMP_ACREFCTL_R &= ~COMP_ACREFCTL_RNG;               // Setting internal reference to high range
+    COMP_ACREFCTL_R |= 0xF;                              // Use internal voltage reference of 2.469
+    COMP_ACCTL0_R |= COMP_ACCTL0_CINV | COMP_ACCTL0_ISEN_RISE | COMP_ACCTL0_ASRCP_REF;  //CINV - invert, ISEN_RISE - RIsing edge, ASRCP_REF - Internal VREF
+}
+
+int main(void)
+{
+    initHW();
+    while(true);
 }
